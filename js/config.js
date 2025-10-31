7e90775e1cdb3044f66eec100dce896e6be10fcc
rlatjdfkr7979-ctrl <rlatjdfkr7979@gmail.com>
2025-11-01 01:54:15 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
    ======================================== */
 
 // Google Forms 설정
-const FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSdOFZRt4o-bw6UNmBB6JtdDQ6PR5f7eW0dpfIs8KwyUysL5ag/viewform?usp=header"; // QR 생성용
+const FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSdGuoHeUW-37RFCBgMH7ZUNL0tt_yHQiIFMrif85mrV428Omg/viewform?usp=header"; // QR 생성용
 const ADD_BOOK_FORM_URL = "https://docs.google.com/forms/d/e/1FAIpQLSdOFZRt4o-bw6UNmBB6JtdDQ6PR5f7eW0dpfIs8KwyUysL5ag/viewform"; // 도서 추가용
 const ENTRY_IDS = {
   code: "entry.32105598",
@@ -39,4 +39,5 @@ window.DOORAY_ENDPOINTS = {
     send: (channelId) => `${window.DOORAY_CONFIG.baseUrl}/v1/messenger/channels/${channelId}/messages`
   }
 };
+
 
